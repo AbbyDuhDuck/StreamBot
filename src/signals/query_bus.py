5573bb82c779c40799b54000c9d163e89a85c97c
Abby Duck <abbyduck96@gmail.com>
2025-10-01 21:40:32 -0400

    Added methods to query buss

--- src/signals/query_bus.py
+++ src/signals/query_bus.py
@@ -19,7 +19,7 @@ TODO
 from typing import Coroutine, Protocol
 from uuid import uuid4, UUID
 
-from .exceptions import DuplicateQueryIDError, QueryNotFoundError, HandlerNotFoundError
+from .exceptions import DuplicateQueryIDError, QueryNotFoundError, QueryIDNotFoundError, HandlerNotFoundError
 
 
 # -=-=- Functions and Classes -=-=- #
@@ -86,54 +86,88 @@ class QueryBus:
     
     def register(self, query:str, handler:Coroutine) -> UUID:
         """register a handler to a query"""
+        id = uuid4()
+        self.set_query_id(id, query)
+        self.set_handler(id, handler)
+        return id
 
     def unregister(self, id:UUID):
         """unregister an event or query"""
+        self.remove_query_id(id)
+        self.remove_handler(id)
 
     # -=-=- Trigger -=-=- #
 
     async def query(self, query:str, data:QueryData) -> Response:
         """trigger a query"""
+        if not self.query_exists(query): raise QueryNotFoundError(query)
+        handler = self.get_query_handler(query)
+        if handler is None: 
+            id = self.get_query_id(query)
+            if id is None: raise QueryIDNotFoundError(id, query)
+            raise HandlerNotFoundError(id, query)
+        return await handler(data)
 
     # -=-=- Exists -=-=- #
 
     def query_exists(self, query:str) -> bool:
         """checks if a query exists"""
+        return query in self.registered
 
     def query_id_exists(self, id:UUID) -> bool:
         """checks if a query id exists"""
+        return self.get_query_name(id) is not None
 
     def handler_exists(self, id:UUID) -> bool:
         """checks if an handler exists for a given id"""
+        return id in self.handlers
 
     # -=-=- Getters and Setters -=-=- #
 
     def get_query_name(self, id:UUID) -> str:
         """get the query name by id"""
+        for query in self.registered:
+            if self.registered[query] is id:
+                return query
 
     def get_query_id(self, query:str) -> UUID:
         """get the query id by name"""
+        return self.registered[query] if self.query_exists(query) else None
+    
+    def get_query_handler(self, query:str) -> Coroutine:
+        """get the query handler by name"""
+        return self.get_handler(self.get_query_id(query))
     
     def get_handler(self, id:UUID) -> Coroutine:
         """get the query handler by its id"""
-        if not self.query_exists(id):
-            raise HandlerNotFoundError(id)
+        if not self.handler_exists(id): return None
         return self.handlers[id]
 
     def set_query_id(self, id:UUID, query:str):
         """set a query id to the current name"""
         # Note: enforce no duplicate IDs
+        if self.query_id_exists(id):
+            current_querry = self.get_query_name(id)
+            if query is not current_querry:
+                raise DuplicateQueryIDError(id, current_querry)
+        self.registered[query] = id
 
     def remove_query_id(self, id:UUID):
         """remove the id from the active queries"""
+        query = self.get_query_name(id)
+        if query is not None:
+            del self.registered[query]
 
     # -=-=- #
 
-    def set_handler(self, id:UUID, callback:Coroutine):
+    def set_handler(self, id:UUID, handler:Coroutine):
         """set the handler for a defined query id"""
+        self.handlers[id] = handler
 
     def remove_handler(self, id:UUID):
         """remove the handler from a defined query id"""
+        if not self.handler_exists(id): return
+        del self.handlers[id]
 
     # -=-=- #
 
@@ -165,7 +199,7 @@ if __name__ == "__main__":
     async def current_song_handler(query: CurrentSongQuery) -> Response:
         # Return a Response object
         song = "Silksong"
-        msg = msg
+        msg = f"{query.user}'s current song is {song}"
         return Response(msg, user=query.user, song=song)
 
     async def main():
