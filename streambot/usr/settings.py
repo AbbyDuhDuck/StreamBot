39a5fdfaa8a4c4c526dafeff91346ebe70cf7165
Abby Duck <abbyduck96@gmail.com>
2026-03-07 11:08:24 -0500

    Add stopping logic to main run loop

--- streambot/usr/settings.py
+++ streambot/usr/settings.py
@@ -17,9 +17,12 @@ TODO
 # -=-=- Imports & Globals -=-=- #
 
 from .events import UserEvents, DefaultEvents
-from .commands import UserCommands, DefaultCommands
 from .services import UserServices
+
+from ..signals import EventBus
+
 import asyncio
+from threading import Thread
 
 
 # -=-=- Functions and Classes -=-=- #
@@ -29,9 +32,10 @@ class UserSettings:
     account_user:str
     account_bot:str|None
 
-    commands:UserCommands=DefaultCommands
     events:UserEvents=DefaultEvents
     services:UserServices
+
+    stop_event:asyncio.Event = asyncio.Event()
 
     def __init__(
             self,
@@ -45,15 +49,43 @@ class UserSettings:
 
         self.services = UserServices(self)
 
+        self.services.enable('sound')
+
+        EventBus.get_instance().register('Stop', EventBus.lambda_action(lambda _: self.stop()))
+
+    def stop(self):
+        self.stop_event.set()
+
     def run(self):
         asyncio.run(self._run_async())
 
     async def _run_async(self):
-        self.commands = self.commands(self)
         self.events = self.events(self)
 
         self.services.register_user_events()
+        print("\nStarting Services...")
         await self.services.start_all()
+        
+        await self.wait_to_stop()
+        
+        print('\nStopping Services...')
+        await self.services.stop_all()
+
+    # -=-=- #
+
+    async def wait_to_stop(self):
+        self.wait_for_enter()
+        await self.stop_event.wait()
+
+    def wait_for_enter(self):
+        # make a daemon thread and call stop if input happens
+        def wait_for_input():
+            input("\nPress Enter to Stop...\n\n")
+            print("Enter Pressed, Stopping...")
+            self.stop()
+            
+        thread = Thread(target=wait_for_input, daemon=True)
+        thread.start()
 
 
 
