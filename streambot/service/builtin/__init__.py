abb05bf3eab0ab3a1d8805448d3888a47015bf1b
Abby Duck <abbyduck96@gmail.com>
2026-03-07 11:11:09 -0500

    import new services

--- streambot/service/builtin/__init__.py
+++ streambot/service/builtin/__init__.py
@@ -16,7 +16,18 @@ TODO
 
 # -=-=- Imports & Globals -=-=- #
 
+from . import gui
+from . import sound
+
 from . import obs
 
+from . import chat, chat_twitch, chat_youtube
+
+from . import commands
+from . import sound_commands
+
+from . import users, data
+
+from . import ai
 
 # EOF #
