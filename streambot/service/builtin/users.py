5823a005ad3e2cd85812bfce80915ed1079c5212
Abby Duck <abbyduck96@gmail.com>
2026-03-23 02:07:25 -0400

    user greetings happen in the users service

--- streambot/service/builtin/users.py
+++ streambot/service/builtin/users.py
@@ -22,11 +22,18 @@ from dataclasses import dataclass, field
 import enum
 from typing import Any, Callable
 
+from .chat import ChatMessageData, ChatMessageOutData, ChatNotificationData, MessageLevel, Platform, UserType
+
 from .. import ConfigClass, configclass, BaseService, serviceclass
 from ...signals import EventBus, EventData, QueryBus, QueryData, Response
 import asyncio
 
 import random
+
+from .chat_twitch import TwitchEventData
+from twitchAPI.object.eventsub import ChannelRaidEvent
+
+from streambot.core.decorators import debounce
 
 # -=-=- Functions & Classes -=-=- #
 
@@ -35,6 +42,10 @@ import random
 
 @configclass
 class UsersConfig(ConfigClass):
+    raid_cooldown:float = 60
+
+    # -=-=- #
+    
     CALLOUTS:list[str] = field(default_factory=[
         "Welcome {user}",
         "Hello {user}",
@@ -79,7 +90,13 @@ class UsersConfig(ConfigClass):
 
 # -=-=- Data Classes -=-=- #
 
-# TODO - dataclasses for event data, query data, etc. related to AI functionality.
+@dataclass
+class UserData:
+    user:str
+
+@dataclass
+class MessageData:
+    message:str
 
 # manage storing user data, user-specific commands, and user-specific events
 
@@ -95,6 +112,11 @@ class UsersService(BaseService[UsersConfig]):
 
     _ignored_users = ["abbyduhduck", 'KofiStreamBot', 'nightbot', 'StreamElements', 'blerp']
 
+    greeting_queue:list[str] = []
+    greeted_users:list[str] = []
+    on_raid_cooldown:bool = False
+    raiders:list[str] = []
+
     async def start(self):
         # print(f"Starting AI Service")
         pass
@@ -105,9 +127,21 @@ class UsersService(BaseService[UsersConfig]):
     
     # -=-=- #
 
-    def __register_events__(self, event_bus):
-        # event_bus.register("ChatMessageIn", self.event_chat_message_in)
-        pass
+    async def say(message:str):
+        await EventBus.get_instance().emit('PlayTTS', MessageData(message))
+
+    async def msg(message:str, user_type:UserType=UserType.BOT, platform:Platform=Platform.TWITCH):
+        await EventBus.get_instance().emit("ChatMessageOut", ChatMessageOutData(message, user_type=user_type, platform=platform))
+
+    async def out(message:str, level:MessageLevel=MessageLevel.INFO):
+        await EventBus.get_instance().emit("ChatNotification", ChatNotificationData(message, level))
+
+    # -=-=- #
+
+    def __register_events__(self, event_bus):        
+        event_bus.register("ChatMessage", self.event_chat_message)
+        event_bus.register('OnGreetUser', self.event_greet_user)
+        event_bus.register('OnTwitchRaidEvent', self.event_raid)
         
     def __register_queries__(self, query_bus):
         query_bus.register("GetUserGreeting", self.query_get_user_greeting)
@@ -140,6 +174,80 @@ class UsersService(BaseService[UsersConfig]):
                 matched[user] = data
         return matched
         
+    # -=-=- #
+
+    @debounce(10)
+    async def greet_users(self):
+        users = self.user_queue
+        self.user_queue = []
+
+        if self.can_greet(*users):
+            users = self.get_valid_users(*users)
+            print(f"Greeting: {self.get_names_string(*users)}")
+            # -=-=- #
+            await self.out(f"Greeting: {self.get_names_string(*users)}")
+            for msg in self.get_greetings(*users):
+                await self.say(msg)
+            self.greeted_users.extend(users)
+
+    # -=-=- #
+
+    def is_custom_user(self, user:str) -> bool:
+        return user in self.greetings
+    
+    def is_returning_user(self, user:str) -> bool:
+        return False
+
+    
+    def can_greet_user(self, user:str) -> bool:
+        return True\
+            and user.strip() != ''\
+            and user not in self.greeted_users\
+            and user not in self._ignored_users
+    
+    def can_greet(self, *users:str) -> bool:
+        return bool(len(self.get_valid_users(*users)))
+
+    def get_valid_users(self, *users:str):
+        usrs = []
+        for user in users:
+            user = user.strip()
+            if user in usrs: continue
+            if user == '': continue
+            if not self.can_greet_user(user): continue
+            usrs.append(user)
+        return usrs
+
+    # -=-=- #
+    
+    def get_called_name(self, user:str) -> str:
+        user = self.how_say.get('user', self.nicknames.get('user', user.lower()))
+        return user.replace('_', ' ')
+    
+    def get_names_string(self, *users) -> str:
+        users = [self.get_called_name(u) for u in users]
+        if len(users) == 1:
+            return users[0]
+        return ', '.join(users[:-1]) + f', and {users[-1]}'
+    
+    # -=-=- #
+
+    def get_greetings(self, *users:str) -> list[str]:
+        users = self.get_valid_users(*users)
+        custom_users = [u for u in users if self.is_custom_user(u)]
+        returning_users = [u for u in users if u not in custom_users and self.is_returning_user(u)]
+        new_users = [u for u in users if u not in [*custom_users, *returning_users]]
+        greetings = []
+        # -=-=- #
+        if len(new_users):
+            greetings.append(self._get_greeting(self.get_names_string(*new_users)))
+        if len(returning_users):
+            greetings.append(self._get_returning_greeting(self.get_names_string(*returning_users)))
+        for user in custom_users:
+            greetings.append(self._get_custom_greeting(user))
+        # -=-=- #
+        return greetings
+
     # -=-=- #
 
     def set_greetings(self, user:str, *greetings:str):
@@ -188,25 +296,48 @@ class UsersService(BaseService[UsersConfig]):
 
     # TODO - add AI functionality here, such as responding to chat messages, generating text, etc.
 
+
     # -=-=- Events -=-=- #
 
-    async def query_get_user_greeting(self, data) -> Response:
+    async def event_chat_message(self, data:ChatMessageData):
+        if not self.can_greet_user(data.user): return
+        if data.platform is not Platform.TWITCH: return
+        if self.on_raid_cooldown and data.user.lower() not in self.raiders: return
+        # -=-=- #
+        self.greeting_queue.append(data.user)
+        await self.greet_users()
+
+    async def event_greet_user(self, data:UserData):
+        greeting, nicknames = self.get_greeting(data.user)
+        await self.say(greeting)
+
+    async def event_raid(self, data:TwitchEventData[ChannelRaidEvent]):
+        self.on_raid_cooldown = True
+        user = data.data.event.from_broadcaster_user_login
+        if user not in self.raiders: self.raiders.append(user)
+        await asyncio.sleep(self.config.raid_cooldown)
+        self.on_raid_cooldown = False
+
+    # -=-=- #
+
+    async def query_get_user_greeting(self, data:UserData) -> Response:
         greeting, nicknames = self.get_greeting(data.user)
         return Response(greeting, greeting=greeting, nicknames=nicknames)
 
-    async def query_get_user_nickname(self, data) -> Response:
+    async def query_get_user_nickname(self, data:MessageData) -> Response:
         nicknames = self.get_matching(data.message)
         return Response(nicknames, nicknames=nicknames)
 
-    async def query_get_nickname(self, data) -> Response:
+    async def query_get_nickname(self, data:UserData) -> Response:
         data = {}
         if data.user in self.nicknames: data['nickname'] = self.nicknames[data.user]
         if data.user in self.how_say: data['how_say'] = self.how_say[data.user]
         return Response(data, nicknames=data)
 
-    async def query_get_lurk_message(self, data) -> Response:
+    async def query_get_lurk_message(self, data:UserData) -> Response:
         message = self.get_lurk_message(data.user)
         return Response(message, message=message)
+    
 
 
 # EOF #
